b9896dcf9966eeab7f84e8f2a1e6845a4610e07d
rbelchenko <rbelchenko@phoenix-it.ru>
2020-09-29 19:02:56 +0300

    fix

--- src/main/kotlin/ru/acvm/eventfilter/EventFilterApplication.kt
+++ src/main/kotlin/ru/acvm/eventfilter/EventFilterApplication.kt
@@ -3,40 +3,25 @@ package ru.acvm.eventfilter
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import ru.acvm.eventfilter.app.DateParser
 import ru.acvm.eventfilter.app.EventFilterService
 import java.io.File
-import java.text.ParseException
-import java.util.*
+import java.lang.Long.parseLong
 
 @SpringBootApplication
-class EventFilterApplication(
-        private val eventFilterService: EventFilterService,
-        private val dateParser: DateParser
-) : CommandLineRunner {
+class EventFilterApplication(private val eventFilterService: EventFilterService) : CommandLineRunner {
     override fun run(vararg args: String?) {
-        if (args.size < 4){
-            println("Arguments: inputFile, outputFile, fromDate, toDate")
+        if (args.size < 3){
+            println("Arguments: inputFile, outputFile, fromTimeKey, [toTimeKey]")
         }
 
         val input = args[0]!!
         val output = args[1]!!
-        val from: Date? = parseDate(args[2]!!)
-        val to: Date? = parseDate(args[3]!!)
+        val from = parseLong(args[2])
+        val to = if (args.size > 3) parseLong(args[3]) else null
 
-        if (from != null && to != null){
-            eventFilterService.filter(File(input), File(output), from, to)
-        }
+        eventFilterService.filter(File(input), File(output), from, to)
     }
 
-    private fun parseDate(text: String): Date? {
-        return try {
-            dateParser.parse(text)
-        } catch (e: ParseException) {
-            println("Date format: dd.MM.yyyy hh:mm")
-            null
-        }
-    }
 }
 
 fun main(args: Array<String>) {
